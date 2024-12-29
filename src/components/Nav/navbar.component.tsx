a3e8914c33cb66f3cc41b794e9c38e3e5459f08b
Kunszt Norbert <kunszt.norbert@gmail.com>
2024-12-29 16:02:18 +0100

    small design in nav

--- src/components/Nav/navbar.component.tsx
+++ src/components/Nav/navbar.component.tsx
@@ -1,24 +1,22 @@
 import {NavLink } from "react-router";
 import { Navitem } from "../../models/Nav";
+import "./navbar.component.scss";
 const Navbar = () => {
   const menuItems: Navitem[] = [
-    { id: 1, name: 'Billing', url: '/billing',icon:"./assets/icons/calculator.svg" },
-    { id: 2, name: 'Statistics', url: '/statistics',icon:"./assets/icons/cash-stack.svg" }
+    { id: 1, name: 'Billing', url: '/billing',icon:"calculator.svg" },
+    { id: 2, name: 'Statistics', url: '/statistics',icon:"cash-stack.svg" }
   ];
-  const listItems = menuItems.map((item) =><>
+  const listItems = menuItems.map((item) =>
     
   <NavLink
     to={item.url}
-    className={({ isActive, isPending }) =>
-      isPending ? "pending" : isActive ? "active" : ""
-    }
+    className="nav-link-item"
   >
-    <img src={item.icon} alt="" /> { item.name}
+    <img src={`./assets/icons/${item.icon}`} alt="" /> { item.name}
   </NavLink>
-  </>
   );
   return (
-    <>{listItems}</>
+    <div className="nav-container">{listItems}</div>
   )
 }
 
