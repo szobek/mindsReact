70e1293c561dcb93e84bf4077f3af372ca3f5c19
Kunszt Norbert <kunszt.norbert@gmail.com>
2025-01-09 16:33:11 +0100

    Use BASE_URL from settings

--- src/components/Billing/BillingWrapper.tsx
+++ src/components/Billing/BillingWrapper.tsx
@@ -3,12 +3,13 @@ import { User } from "../../models/User";
 import CardWrapper from "./CardWrapper";
 import BilllingHeader from "./Header";
 import InvoiceListTable from "./InvoiceList";
+import { settings } from "../../settings";
 
 const BillingWrapper = () => {
     document.title = "Billing"
     const [userData, setUserData] = useState<User | null>(null)
     useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users/5')
+        fetch(`${settings.BASE_URL}/users/5`)
             .then(response => response.json())
             .then(json => setUserData(json as User))
             .catch(error => console.error(error))
